3158afea7b3755aa1bdb818c608bf851943c6d13
Harsh <harshsingh3776@gmail.com>
2025-01-10 14:10:49 +0530

    added solution

---
+++ leetcode_solutions/217_contains_duplicate.cpp
@@ -0,0 +1,19 @@
+//217. Contains Duplicate
+// Runtime: 27ms, memory usage: 61.02MB
+
+
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int size = nums.size();
+        sort(nums.begin(),nums.end());
+        
+        for(int i = 0; i < size-1; ++i){
+            if(nums[i]==nums[i+1])
+                return true;
+        }
+
+        return false;
+    }
+};
