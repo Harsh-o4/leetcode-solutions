7d5743e3a6bdeff28ca6778266dbc48ea72a9959
Harsh <harshsingh3776@gmail.com>
2025-01-15 10:39:59 +0530

    added solution

---
+++ leetcode_solutions/1207_unique_number_of_occurences.cpp
@@ -0,0 +1,24 @@
+//1207. Unique Number of Occurences
+// Runtime: 0 ms, Memory Usage: 11.98 MB
+
+
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        map<int,int> freq;
+
+        for(int val:arr){
+            ++freq[val];
+        }
+
+        map<int,int> unique;
+        for(const auto& [key,value]:freq){
+            ++unique[value];
+
+            if(unique[value]==2) return false;
+        }
+
+        return true;
+    }
+};
